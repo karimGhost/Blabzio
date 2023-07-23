266da2ae6624b58def7e44408ad6bad96ec45c0d
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-07-23 12:49:46 -0400

    useCallback init

--- src/pages/news/Posted/Comments.js
+++ src/pages/news/Posted/Comments.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect} from "react";
+import React, { useState, useRef, useEffect, useCallback} from "react";
 import ReactHtmlParser, { Comment } from "html-react-parser";
 import { Checkbox } from "primereact/checkbox";
 import { SelectButton } from 'primereact/selectbutton';
