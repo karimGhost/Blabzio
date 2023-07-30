82814c1318dd96f25012364ec1696b6aa65032d8
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-07-30 17:58:43 +0000

    RecordRTC

--- src/pages/news/VideoUploader.js
+++ src/pages/news/VideoUploader.js
@@ -24,13 +24,19 @@ const firebaseConfig121212 = {
 }
 
 
+function VideoUploader(){
+
+
+
+
+
+
 firebase.initializeApp(firebaseConfig121212, 'app121212');
 
 const app004 = firebase.app('app121212');
 
 const database = app004.database();
 
-function VideoUploader() {
   const [videos, setVideos] = useState([]);
   const [recording, setRecording] = useState(false);
   const [mediaBlobUrl, setMediaBlobUrl] = useState('');
@@ -116,8 +122,95 @@ function VideoUploader() {
   };
 
   return (
- <div>
-  </div>
+    <div className="container mt-5">
+      <h1 className="mb-4">Video Recorder</h1>
+      <div>
+        {recording ? (
+          <button className="btn btn-danger mr-2" onClick={handleStopRecording}>
+            Stop Recording
+          </button>
+        ) : (
+          <button className="btn btn-primary mr-2" onClick={handleStartRecording}>
+            Start Recording
+          </button>
+        )}
+        {recording ? null : (
+          <button
+            className="btn btn-success mr-2"
+            onClick={() => setVideos((prevVideos) => [...prevVideos, mediaBlobUrl])}
+            disabled={!mediaBlobUrl}
+          >
+            Upload
+          </button>
+        )}
+      </div>
+      <video
+        ref={videoRef}
+        src={mediaBlobUrl}
+        style={{ width: '100%', marginBottom: '10px' }}
+        autoPlay
+        controls
+      />
+      <hr />
+      {videos.map((video, index) => (
+        <div key={index} className="card mb-3">
+          <div className="card-body">
+            <video
+              src={video.url}
+              controls
+              style={{ width: '100%', cursor: 'pointer' }}
+              onMouseEnter={(e) => e.target.play()}
+              onMouseLeave={(e) => e.target.pause()}
+            />
+            <div className="text-muted mt-2">
+              Likes: {video.likes || 0} | Dislikes: {video.dislikes || 0}
+            </div>
+            <div className="my-3">
+              <button className="btn btn-sm btn-primary mr-2" onClick={() => handleLike(video.id)}>
+                Like
+              </button>
+              <button className="btn btn-sm btn-danger" onClick={() => handleDislike(video.id)}>
+                Dislike
+              </button>
+            </div>
+            <div>
+              <h5>Comments:</h5>
+              <ul className="list-unstyled">
+                {Object.values(video.comments || []).map((comment) => (
+                  <li key={comment.id}>{comment.text}</li>
+                ))}
+              </ul>
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Add a comment..."
+                  aria-label="Add a comment"
+                  aria-describedby="button-addon2"
+                  value={commentInput}
+                  onChange={(e) => setCommentInput(e.target.value)}
+                />
+                <div className="input-group-append">
+                  <button
+                    className="btn btn-outline-secondary"
+                    type="button"
+                    id="button-addon2"
+                    onClick={() => handleComment(video.id)}
+                  >
+                    Post
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div>
+              <button className="btn btn-danger btn-sm" onClick={() => handleDelete(video.id)}>
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
 
