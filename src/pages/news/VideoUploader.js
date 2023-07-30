ef960e93b594287f775958a0b5a4983d330584a3
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-07-30 18:44:03 +0000

    videp

--- src/pages/news/VideoUploader.js
+++ src/pages/news/VideoUploader.js
@@ -42,100 +42,22 @@ const database = app3333.database();
   const videoRef = useRef(null);
   const recorderRef = useRef(null);
 
-  const handleStartRecording = () => {
-    setRecording(true);
-    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-      videoRef.current.srcObject = stream;
-     // recorderRef.current = RecordRTC(stream, { type: 'video' });
-      recorderRef.current.startRecording();
-    });
-  };
-
-  const handleStopRecording = () => {
-    setRecording(false);
-    recorderRef.current.stopRecording(() => {
-      const blob = recorderRef.current.getBlob();
-      const videoId = nanoid();
-      const storageRef = database.ref();
-      const videoRef = storageRef.child('videos/' + videoId + '.webm');
-      videoRef.put(blob).then(() => {
-        videoRef.getDownloadURL().then((downloadURL) => {
-          const videoData = {
-            id: videoId,
-            url: downloadURL,
-            likes: 0,
-            dislikes: 0,
-            comments: [],
-          };
-          database.ref('videos/' + videoId).set(videoData);
-          setVideos((prevVideos) => [...prevVideos, videoData]);
-        });
-      });
-    });
-  };
-
-  const handleLike = (videoId) => {
-    database
-      .ref('videos/' + videoId)
-      .transaction((video) => {
-        if (video) {
-          video.likes = (video.likes || 0) + 1;
-        }
-        return video;
-      });
-  };
-
-  const handleDislike = (videoId) => {
-    database
-      .ref('videos/' + videoId)
-      .transaction((video) => {
-        if (video) {
-          video.dislikes = (video.dislikes || 0) + 1;
-        }
-        return video;
-      });
-  };
-
-  const handleComment = (videoId) => {
-    if (commentInput.trim() === '') {
-      return;
-    }
-
-    const newComment = {
-      id: nanoid(),
-      text: commentInput.trim(),
-    };
-
-    database.ref('videos/' + videoId + '/comments').push(newComment);
-    setCommentInput('');
-  };
-
-  const handleDelete = (videoId) => {
-    const confirmDelete = window.confirm('Are you sure you want to delete this video?');
-    if (confirmDelete) {
-      database.ref('videos/' + videoId).remove();
-      setVideos((prevVideos) => prevVideos.filter((video) => video.id !== videoId));
-    }
-  };
-
   return (
     <div className="container mt-5">
       <h1 className="mb-4">Video Recorder</h1>
       <div>
         {recording ? (
-          <button className="btn btn-danger mr-2" onClick={handleStopRecording}>
+          <button className="btn btn-danger mr-2" >
             Stop Recording
           </button>
         ) : (
-          <button className="btn btn-primary mr-2" onClick={handleStartRecording}>
+          <button className="btn btn-primary mr-2" >
             Start Recording
           </button>
         )}
         {recording ? null : (
           <button
             className="btn btn-success mr-2"
-            onClick={() => setVideos((prevVideos) => [...prevVideos, mediaBlobUrl])}
-            disabled={!mediaBlobUrl}
           >
             Upload
           </button>
@@ -143,39 +65,35 @@ const database = app3333.database();
       </div>
       <video
         ref={videoRef}
-        src={mediaBlobUrl}
+        src={""}
         style={{ width: '100%', marginBottom: '10px' }}
         autoPlay
         controls
       />
       <hr />
-      {videos.map((video, index) => (
+  
         <div key={index} className="card mb-3">
           <div className="card-body">
             <video
-              src={video.url}
+              src={""}
               controls
               style={{ width: '100%', cursor: 'pointer' }}
-              onMouseEnter={(e) => e.target.play()}
-              onMouseLeave={(e) => e.target.pause()}
+           
             />
             <div className="text-muted mt-2">
-              Likes: {video.likes || 0} | Dislikes: {video.dislikes || 0}
             </div>
             <div className="my-3">
-              <button className="btn btn-sm btn-primary mr-2" onClick={() => handleLike(video.id)}>
+              <button className="btn btn-sm btn-primary mr-2">
                 Like
               </button>
-              <button className="btn btn-sm btn-danger" onClick={() => handleDislike(video.id)}>
+              <button className="btn btn-sm btn-danger" >
                 Dislike
               </button>
             </div>
             <div>
               <h5>Comments:</h5>
               <ul className="list-unstyled">
-                {Object.values(video.comments || []).map((comment) => (
-                  <li key={comment.id}>{comment.text}</li>
-                ))}
+              
               </ul>
               <div className="input-group mb-3">
                 <input
@@ -184,15 +102,14 @@ const database = app3333.database();
                   placeholder="Add a comment..."
                   aria-label="Add a comment"
                   aria-describedby="button-addon2"
-                  value={commentInput}
-                  onChange={(e) => setCommentInput(e.target.value)}
+               
                 />
                 <div className="input-group-append">
                   <button
                     className="btn btn-outline-secondary"
                     type="button"
                     id="button-addon2"
-                    onClick={() => handleComment(video.id)}
+                    
                   >
                     Post
                   </button>
@@ -200,13 +117,13 @@ const database = app3333.database();
               </div>
             </div>
             <div>
-              <button className="btn btn-danger btn-sm" onClick={() => handleDelete(video.id)}>
+              <button className="btn btn-danger btn-sm">
                 Delete
               </button>
             </div>
           </div>
         </div>
-      ))}
+      
     </div>
   );
 }
