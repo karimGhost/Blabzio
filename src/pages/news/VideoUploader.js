7e33027cf7b6b37ab7bf4fb2c176880510dfcaa0
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-07-30 19:39:13 +0000

    VideoUploader

--- src/pages/news/VideoUploader.js
+++ src/pages/news/VideoUploader.js
@@ -1,67 +1,120 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+
+import { nanoid } from 'nanoid';
+import RecordRTC from 'recordrtc';
 
 
-  
-class VideoUploader extends React.Component {
 
-  
+function VideoUploader() {
+  const [videos, setVideos] = useState([]);
+  const [recording, setRecording] = useState(false);
+  const [mediaBlobUrl, setMediaBlobUrl] = useState('');
+  const [commentInput, setCommentInput] = useState('');
 
-  render() {
+  const videoRef = useRef(null);
+  const recorderRef = useRef(null);
 
-    return (
-      <div className="container mt-5">
+  const handleStartRecording = () => {
+    setRecording(true);
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+      videoRef.current.srcObject = stream;
+      recorderRef.current = RecordRTC(stream, { type: 'video' });
+      recorderRef.current.startRecording();
+    });
+  };
+
+  const handleStopRecording = () => {
+    setRecording(false);
+   
+   
+  };
+
+  const handleLike = (videoId) => {
+    
+  };
+
+  const handleDislike = (videoId) => {
+    
+  };
+
+  const handleComment = (videoId) => {
+    if (commentInput.trim() === '') {
+      return;
+    }
+
+    const newComment = {
+      id: nanoid(),
+      text: commentInput.trim(),
+    };
+
+    setCommentInput('');
+  };
+
+  const handleDelete = (videoId) => {
+    const confirmDelete = window.confirm('Are you sure you want to delete this video?');
+    if (confirmDelete) {
+    
+    }
+  };
+
+  return (
+    <div className="container mt-5">
       <h1 className="mb-4">Video Recorder</h1>
       <div>
         {recording ? (
-          <button className="btn btn-danger mr-2" >
+          <button className="btn btn-danger mr-2" onClick={handleStopRecording}>
             Stop Recording
           </button>
         ) : (
-          <button className="btn btn-primary mr-2">
+          <button className="btn btn-primary mr-2" onClick={handleStartRecording}>
             Start Recording
           </button>
         )}
-       
+        {recording ? null : (
           <button
             className="btn btn-success mr-2"
+            onClick={() => setVideos((prevVideos) => [...prevVideos, mediaBlobUrl])}
+            disabled={!mediaBlobUrl}
           >
             Upload
           </button>
-      
+        )}
       </div>
       <video
-     
-        src=""
+        ref={videoRef}
+        src={mediaBlobUrl}
         style={{ width: '100%', marginBottom: '10px' }}
         autoPlay
         controls
       />
       <hr />
+      {videos.map((video, index) => (
         <div key={index} className="card mb-3">
           <div className="card-body">
             <video
-              src=""
+              src={video.url}
               controls
               style={{ width: '100%', cursor: 'pointer' }}
-            
+              onMouseEnter={(e) => e.target.play()}
+              onMouseLeave={(e) => e.target.pause()}
             />
             <div className="text-muted mt-2">
-
-lik            </div>
+              Likes: {video.likes || 0} | Dislikes: {video.dislikes || 0}
+            </div>
             <div className="my-3">
-              <button className="btn btn-sm btn-primary mr-2" >
+              <button className="btn btn-sm btn-primary mr-2" onClick={() => handleLike(video.id)}>
                 Like
               </button>
-              <button className="btn btn-sm btn-danger" >
+              <button className="btn btn-sm btn-danger" onClick={() => handleDislike(video.id)}>
                 Dislike
               </button>
             </div>
             <div>
               <h5>Comments:</h5>
               <ul className="list-unstyled">
-               
-                  <li>jjjj</li>
-               
+                {Object.values(video.comments || []).map((comment) => (
+                  <li key={comment.id}>{comment.text}</li>
+                ))}
               </ul>
               <div className="input-group mb-3">
                 <input
@@ -70,15 +123,15 @@ lik            </div>
                   placeholder="Add a comment..."
                   aria-label="Add a comment"
                   aria-describedby="button-addon2"
-                  value=""
-                
+                  value={commentInput}
+                  onChange={(e) => setCommentInput(e.target.value)}
                 />
                 <div className="input-group-append">
                   <button
                     className="btn btn-outline-secondary"
                     type="button"
                     id="button-addon2"
-                   
+                    onClick={() => handleComment(video.id)}
                   >
                     Post
                   </button>
@@ -86,16 +139,15 @@ lik            </div>
               </div>
             </div>
             <div>
-              <button className="btn btn-danger btn-sm" >
+              <button className="btn btn-danger btn-sm" onClick={() => handleDelete(video.id)}>
                 Delete
               </button>
             </div>
           </div>
         </div>
-      
+      ))}
     </div>
-    );
-  }
+  );
 }
 
 export default VideoUploader;
