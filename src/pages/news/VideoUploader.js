5ad661d51641dc0e507bd7428eaea5d54097125a
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-07-30 19:15:28 +0000

    ref

--- src/pages/news/VideoUploader.js
+++ src/pages/news/VideoUploader.js
@@ -1,36 +1,11 @@
 import React, { useState, useRef } from 'react';
-import 'firebase/compat/app';
 
 import { nanoid } from 'nanoid';
 import RecordRTC from 'recordrtc';
-import firebase from 'firebase/compat/app';
 import { useAuth } from '../../Accounts/useAuth';
-const firebaseConfig3333 = {
-  apiKey: "AIzaSyChFGTB5YEugUKho-YqcWVZtKJG3PIrtt0",
-
-  authDomain: "thewall-10a4a.firebaseapp.com",
-
-  databaseURL: "https://thewall-10a4a-default-rtdb.firebaseio.com",
-
-  projectId: "thewall-10a4a",
-
-  storageBucket: "thewall-10a4a.appspot.com",
-
-  messagingSenderId: "221023885061",
-
-  appId: "1:221023885061:web:bc550d03edd2fbf60e496c",
-
-  measurementId: "G-7V80059NF7"
-}
 
 
 function VideoUploader(){
-
-firebase.initializeApp(firebaseConfig3333, 'app3333');
-
-const { user,setUser, loading,signOut } = useAuth();
-const app3333 = firebase.app('app3333');
-
 
 
   const [videos, setVideos] = useState([]);
