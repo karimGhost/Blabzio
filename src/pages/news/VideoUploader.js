4c5bdc6d882243ad6eb8bc7245c2508f6d20c2e3
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-07-30 20:18:46 +0000

    rtc

--- src/pages/news/VideoUploader.js
+++ src/pages/news/VideoUploader.js
@@ -4,6 +4,7 @@ import 'firebase/compat/database';
 import 'firebase/compat/storage';
 import { nanoid } from 'nanoid';
 import firebase from 'firebase/compat/app';
+import RecordRTC, { RecordRTCPromisesHandler } from "recordrtc";
 
 const firebaseConfig121212 = {
   apiKey: "AIzaSyChFGTB5YEugUKho-YqcWVZtKJG3PIrtt0",
