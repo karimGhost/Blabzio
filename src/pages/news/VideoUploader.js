4ac38ba099218cd212d3a9855efde9292e5e8d7a
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-07-30 15:49:29 +0000

    authDomain

--- src/pages/news/VideoUploader.js
+++ src/pages/news/VideoUploader.js
@@ -4,10 +4,7 @@ import { nanoid } from 'nanoid';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/storage'; // Import storage module
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useMediaRecorder } from 'react-media-recorder'; // Import react-media-recorder
-
-// Your Firebase configuration
-// ... (existing Firebase configuration)
+import { useReactMediaRecorder } from 'react-media-recorder'; // Import react-media-recorder
 const firebaseConfig121212 = {
   apiKey: "AIzaSyChFGTB5YEugUKho-YqcWVZtKJG3PIrtt0",
 
@@ -30,21 +27,24 @@ firebase.initializeApp(firebaseConfig121212, 'app121212');
 const app4 = firebase.app('app121212');
 
   const database = app4.database(); 
+const VideoRecorder = () => {
+  const {
+    status,
+    startRecording,
+    stopRecording,
+    mediaBlobUrl,
+    previewStream,
+    clearBlobUrl,
+  } = useReactMediaRecorder({ video: true, audio: true });
 
-class VideoRecorder extends React.Component {
-  state = {
-    videoFile: null,
-    isRecording: false,
-    recordedVideo: null,
-    videos: [],
-    commentInput: "",
-  };
+  const [videos, setVideos] = React.useState([]);
+  const [commentInput, setCommentInput] = React.useState("");
 
-  componentDidMount() {
-    this.loadVideos();
-  }
+  React.useEffect(() => {
+    loadVideos();
+  }, []);
 
-  loadVideos = () => {
+  const loadVideos = () => {
     const videosRef = database.ref('videos');
     videosRef.on('value', (snapshot) => {
       const videos = [];
@@ -55,60 +55,27 @@ class VideoRecorder extends React.Component {
         };
         videos.push(video);
       });
-      this.setState({ videos });
+      setVideos(videos);
     });
   };
 
-  handleFileChange = (e) => {
+  const handleFileChange = (e) => {
     const file = e.target.files[0];
     if (file && file.size < 5000000) { // Maximum allowed file size is 5MB
-      this.setState({ videoFile: file });
+      // Handle the file here
     } else {
       alert('Please select a short video file (less than 5MB).');
     }
   };
 
-  handleVideoRecorded = (recordedBlob) => {
-    this.setState({ recordedVideo: recordedBlob });
-  };
-
-  handleRecordStart = () => {
-    this.setState({ isRecording: true });
-    navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-      .then((stream) => {
-        const mediaRecorder = new MediaRecorder(stream);
-        const chunks = [];
-
-        mediaRecorder.addEventListener('dataavailable', (event) => {
-          chunks.push(event.data);
-        });
-
-        mediaRecorder.addEventListener('stop', () => {
-          const recordedBlob = new Blob(chunks, { type: 'video/webm' });
-          this.setState({ recordedVideo: recordedBlob });
-        });
-
-        mediaRecorder.start();
-        this.setState({ mediaRecorder, recordedChunks: chunks });
-      })
-      .catch((error) => {
-        console.error('Error accessing video stream:', error);
-      });
-  };
-
-  handleRecordStop = () => {
-    this.state.mediaRecorder.stop();
-    this.setState({ isRecording: false });
-  };
-
-  handleUploadClick = () => {
-    const { videoFile, recordedVideo } = this.state;
-    const videoToUpload = videoFile || recordedVideo;
-
-    if (videoToUpload) {
+  const handleUploadClick = () => {
+    const { mediaBlob, mediaBlobUrl } = useReactMediaRecorder;
+    if (mediaBlobUrl) {
+      // Handle the mediaBlob and mediaBlobUrl here
+      // For example, upload the mediaBlob to Firebase Storage and save the URL to the database
       const storageRef = firebase.storage().ref();
-      const videoRef = storageRef.child(videoToUpload.name || 'recorded_video.webm');
-      const uploadTask = videoRef.put(videoToUpload);
+      const videoRef = storageRef.child('recorded_video.webm');
+      const uploadTask = videoRef.put(mediaBlob);
 
       uploadTask.on(
         'state_changed',
@@ -140,7 +107,7 @@ class VideoRecorder extends React.Component {
     }
   };
 
-  handleLike = (videoId) => {
+  const handleLike = (videoId) => {
     database.ref('videos/' + videoId).transaction((video) => {
       if (video) {
         video.likes = (video.likes || 0) + 1;
@@ -149,7 +116,7 @@ class VideoRecorder extends React.Component {
     });
   };
 
-  handleDislike = (videoId) => {
+  const handleDislike = (videoId) => {
     database.ref('videos/' + videoId).transaction((video) => {
       if (video) {
         video.dislikes = (video.dislikes || 0) + 1;
@@ -158,113 +125,109 @@ class VideoRecorder extends React.Component {
     });
   };
 
-  handleComment = (videoId, comment) => {
+  const handleComment = (videoId, comment) => {
     const newComment = {
       id: nanoid(),
       text: comment,
     };
 
     database.ref('videos/' + videoId + '/comments').push(newComment);
-    this.setState({ commentInput: "" }); // Clear the comment input after posting
+    setCommentInput(""); // Clear the comment input after posting
   };
 
-  handleDelete = (videoId) => {
+  const handleDelete = (videoId) => {
     const confirmDelete = window.confirm('Are you sure you want to delete this video?');
     if (confirmDelete) {
       database.ref('videos/' + videoId).remove();
     }
   };
 
-  render() {
-    const { videos, isRecording } = this.state;
-
-    return (
-      <div className="container">
-        <h1>Video Uploader</h1>
-        <div className="form-group">
-          {isRecording ? (
-            <button className="btn btn-danger" onClick={this.handleRecordStop}>
-              Release to Stop Recording
-            </button>
-          ) : (
-            <button className="btn btn-primary" onClick={this.handleRecordStart}>
-              Click and Hold to Record
-            </button>
-          )}
-        </div>
-        {this.state.recordedVideo && (
-          <video src={URL.createObjectURL(this.state.recordedVideo)} autoPlay controls />
-        )}
-        <div className="form-group">
-          <input type="file" accept="video/*" onChange={this.handleFileChange} />
-        </div>
-        <div className="form-group">
-          <button className="btn btn-success" onClick={this.handleUploadClick}>
-            Upload
+  return (
+    <div className="container">
+      <h1>Video Uploader</h1>
+      <div className="form-group">
+        {status === 'recording' ? (
+          <button className="btn btn-danger" onClick={stopRecording}>
+            Release to Stop Recording
           </button>
-        </div>
-        <hr />
-        {videos.map((video) => (
-          <div key={video.id} className="card mb-3">
-            <div className="card-body">
-              <video
-                src={video.url}
-                controls
-                style={{ width: '100%', cursor: 'pointer' }}
-                onMouseEnter={(e) => e.target.play()}
-                onMouseLeave={(e) => e.target.pause()}
-              />
-              <div className="text-muted mt-2">
-                Likes: {video.likes || 0} | Dislikes: {video.dislikes || 0}
-              </div>
-              <div className="my-3">
-                <button className="btn btn-sm btn-primary mr-2" onClick={() => this.handleLike(video.id)}>
-                  Like
-                </button>
-                <button className="btn btn-sm btn-danger" onClick={() => this.handleDislike(video.id)}>
-                  Dislike
-                </button>
-              </div>
-              <div>
-                <h5>Comments:</h5>
-                <ul className="list-unstyled">
-                  {Object.values(video.comments || []).map((comment) => (
-                    <li key={comment.id}>{comment.text}</li>
-                  ))}
-                </ul>
-                <div className="input-group mb-3">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Add a comment..."
-                    aria-label="Add a comment"
-                    aria-describedby="button-addon2"
-                    value={this.state.commentInput}
-                    onChange={(e) => this.setState({ commentInput: e.target.value })}
-                  />
-                  <div className="input-group-append">
-                    <button
-                      className="btn btn-outline-secondary"
-                      type="button"
-                      id="button-addon2"
-                      onClick={() => this.handleComment(video.id, this.state.commentInput)}
-                    >
-                      Post
-                    </button>
-                  </div>
+        ) : (
+          <button className="btn btn-primary" onClick={startRecording}>
+            Click and Hold to Record
+          </button>
+        )}
+      </div>
+      {mediaBlobUrl && (
+        <video src={mediaBlobUrl} autoPlay controls />
+      )}
+      <div className="form-group">
+        <input type="file" accept="video/*" onChange={handleFileChange} />
+      </div>
+      <div className="form-group">
+        <button className="btn btn-success" onClick={handleUploadClick}>
+          Upload
+        </button>
+      </div>
+      <hr />
+      {videos.map((video) => (
+        <div key={video.id} className="card mb-3">
+          <div className="card-body">
+            <video
+              src={video.url}
+              controls
+              style={{ width: '100%', cursor: 'pointer' }}
+              onMouseEnter={(e) => e.target.play()}
+              onMouseLeave={(e) => e.target.pause()}
+            />
+            <div className="text-muted mt-2">
+              Likes: {video.likes || 0} | Dislikes: {video.dislikes || 0}
+            </div>
+            <div className="my-3">
+              <button className="btn btn-sm btn-primary mr-2" onClick={() => handleLike(video.id)}>
+                Like
+              </button>
+              <button className="btn btn-sm btn-danger" onClick={() => handleDislike(video.id)}>
+                Dislike
+              </button>
+            </div>
+            <div>
+              <h5>Comments:</h5>
+              <ul className="list-unstyled">
+                {Object.values(video.comments || []).map((comment) => (
+                  <li key={comment.id}>{comment.text}</li>
+                ))}
+              </ul>
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Add a comment..."
+                  aria-label="Add a comment"
+                  aria-describedby="button-addon2"
+                  value={commentInput}
+                  onChange={(e) => setCommentInput(e.target.value)}
+                />
+                <div className="input-group-append">
+                  <button
+                    className="btn btn-outline-secondary"
+                    type="button"
+                    id="button-addon2"
+                    onClick={() => handleComment(video.id, commentInput)}
+                  >
+                    Post
+                  </button>
                 </div>
               </div>
-              <div>
-                <button className="btn btn-danger btn-sm" onClick={() => this.handleDelete(video.id)}>
-                  Delete
-                </button>
-              </div>
+            </div>
+            <div>
+              <button className="btn btn-danger btn-sm" onClick={() => handleDelete(video.id)}>
+                Delete
+              </button>
             </div>
           </div>
-        ))}
-      </div>
-    );
-  }
-}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default VideoRecorder;
