4632309d82a491284a1592433720195676c5a67b
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-07-30 20:06:49 +0000

    rtc

--- src/pages/news/VideoUploader.js
+++ src/pages/news/VideoUploader.js
@@ -4,6 +4,7 @@ import 'firebase/compat/database';
 import 'firebase/compat/storage';
 import { nanoid } from 'nanoid';
 import firebase from 'firebase/compat/app';
+import RecordRTC from 'recordrtc';
 const firebaseConfig121212 = {
   apiKey: "AIzaSyChFGTB5YEugUKho-YqcWVZtKJG3PIrtt0",
 
@@ -36,9 +37,11 @@ const VideoUploader = () => {
   const videoRef = useRef(null);
   const recorderRef = useRef(null);
 
-  const handleStartRecording = () => {
+  
+
+  const handleStartRecording = async () => {
     setRecording(true);
- 
+   
   };
 
   const handleStopRecording = () => {
