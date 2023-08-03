8b760d3993469ab205fc85a65c4686db5b01a545
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-08-03 22:35:07 +0300

    Update VideoUploader.js

--- src/pages/news/VideoUploader.js
+++ src/pages/news/VideoUploader.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useReactMediaRecorder } from "react-media-recorder"; // Correct import
 
 const VideoUploader = () => {
   const [useReactMediaRecorder, setUseReactMediaRecorder] = useState(null);
