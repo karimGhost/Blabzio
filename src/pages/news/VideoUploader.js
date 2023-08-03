476c39b3f5f4f7e27d6d38daa5ed6ff4603b8cec
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-08-03 23:36:23 +0300

    Update VideoUploader.js

--- src/pages/news/VideoUploader.js
+++ src/pages/news/VideoUploader.js
@@ -1,52 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { MediaRecorder } from "mediarecorder";
+import { ReactMediaRecorder } from "react-media-recorder";
 
-const VideoUploader = () => {
-  const [isRecording, setIsRecording] = useState(false);
-  const [mediaBlobUrl, setMediaBlobUrl] = useState(null);
-
-  const recorder = new MediaRecorder();
-
-  useEffect(() => {
-    // Start recording when the button is clicked
-    const startRecording = () => {
-      recorder.start();
-    };
-
-    // Stop recording when the button is clicked
-    const stopRecording = () => {
-      recorder.stop();
-    };
-
-    // Handle the recording state
-    const handleRecordingState = (event) => {
-      setIsRecording(event.state === "recording");
-    };
-
-    // Handle the media blob URL
-    const handleMediaBlobUrl = (event) => {
-      setMediaBlobUrl(event.dataUrl);
-    };
-
-    // Bind the event handlers
-    recorder.onstart = startRecording;
-    recorder.onstop = stopRecording;
-    recorder.ondataavailable = handleMediaBlobUrl;
-
-    return () => {
-      // Clean up the recorder when the component unmounts
-      recorder.stop();
-    };
-  }, []);
-
-  return (
-    <div>
-      <p>{isRecording ? "Recording..." : "Not recording..."}</p>
-      <button onClick={startRecording}>Start Recording</button>
-      <button onClick={stopRecording}>Stop Recording</button>
-      <video src={mediaBlobUrl} controls autoPlay loop />
-    </div>
-  );
-};
-
-export default VideoUploader;
+export default const VideoUploader = () => (
+  <div>
+    <ReactMediaRecorder
+      video
+      render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+        <div>
+          <p>{status}</p>
+          <button onClick={startRecording}>Start Recording</button>
+          <button onClick={stopRecording}>Stop Recording</button>
+          <video src={mediaBlobUrl} controls autoPlay loop />
+        </div>
+      )}
+    />
+  </div>
+);
