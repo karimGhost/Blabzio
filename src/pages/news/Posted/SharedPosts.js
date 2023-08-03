17482ac32e924e663c55b2df98cdddf9d7da3250
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-08-03 22:24:28 +0300

    Update SharedPosts.js

--- src/pages/news/Posted/SharedPosts.js
+++ src/pages/news/Posted/SharedPosts.js
@@ -1,69 +1,8 @@
 import React, { useRef, useState, useEffect } from 'react';
-
-  // Wrap the import in a dynamic import() to handle SSR
-  const ReactMediaRecorderDynamic = React.lazy(() => import('react-media-recorder'));
-  
-  const VideoPreview = ({ stream }) => {
-    const videoRef = useRef(null);
-  
-    useEffect(() => {
-      if (videoRef.current && stream) {
-        videoRef.current.srcObject = stream;
-      }
-    }, [stream]);
-  
-    if (!stream) {
-      return null;
-    }
-  
-    return <video ref={videoRef} width={500} height={500} autoPlay controls />;
-  };
-  
-  export default function SharedPosts() {
-    const [enable, setEnable] = useState(false); // Set to 'false' initially
-  
-    useEffect(() => {
-      setEnable(true); // Set 'enable' to 'true' on the client-side
-    }, []);
-  
-    return (
-      <div>
-        <h1>Hello StackBlitz!</h1>
-        <div>
-          {/* Use ReactMediaRecorderDynamic instead of ReactMediaRecorder */}
-          <React.Suspense fallback={<div>Loading...</div>}>
-            <ReactMediaRecorderDynamic
-              // Other props...
-              render={({ previewStream, status, startRecording, stopRecording, mediaBlobUrl }) => {
-                console.log(previewStream);
-                const isRecording = status === 'recording';
-  
-                return (
-                  <div>
-                    <p>{status}</p>
-                    <button onClick={startRecording}>Start Recording</button>
-                    <button onClick={stopRecording}>Stop Recording</button>
-                    <button
-                      onClick={() => {
-                        startRecording();
-                        setTimeout(() => {
-                          stopRecording();
-                          setEnable(false);
-                        }, 2000);
-                      }}
-                    >
-                      togglestreaming
-                    </button>
-                    {isRecording && <video src={mediaBlobUrl} controls autoPlay loop />}
-                    {isRecording && <VideoPreview stream={previewStream} />}
-                  </div>
-                );
-              }}
-            />
-          </React.Suspense>
-        </div>
-      </div>
-    );
-  }
-  
-
+export default function SharedPosts(){
+  return (
+    <div>
+    
+    </div>
+  );
+}
