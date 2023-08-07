b0c96e00f8d0b7886de7b82a323ae4b73d36cdb0
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-08-07 19:42:55 +0000

    commit

---
+++ public/component---src-pages-news-posted-video-js-js-c6df1b4d7662cf96ff7e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkblabzio=self.webpackChunkblabzio||[]).push([[7594],{3395:function(e,l,a){a.r(l);var u=a(7294);l.default=()=>u.createElement("div",null)}}]);
+//# sourceMappingURL=component---src-pages-news-posted-video-js-js-c6df1b4d7662cf96ff7e.js.map
--- src/pages/news/VideoUploader.js
+++ src/pages/news/VideoUploader.js
@@ -1,62 +1,10 @@
-import React, { useState, useRef, useEffect } from 'react';
-import RecordRTC, { invokeSaveAsDialog } from 'recordrtc';
+import React from 'react';
 
-export default function VideoUploader() {
-  const [stream, setStream] = useState(null);
-  const [blob, setBlob] = useState(null);
-  const refVideo = useRef(null);
-  const recorderRef = useRef(null);
+function VideoUploader(){
+  return(
 
-  const handleRecording = async () => {
-    // const cameraStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-    const mediaStream = await navigator.mediaDevices.getDisplayMedia({
-      video: {
-        width: 1920,
-        height: 1080,
-        frameRate: 30,
-      },
-      audio: false,
-    });
+    <div> go</div>
+  )
 
-    setStream(mediaStream);
-    recorderRef.current = new RecordRTC(mediaStream, { type: 'video' });
-    recorderRef.current.startRecording();
-  };
-
-  const handleStop = () => {
-    recorderRef.current.stopRecording(() => {
-      setBlob(recorderRef.current.getBlob());
-    });
-  };
-
-  const handleSave = () => {
-    invokeSaveAsDialog(blob);
-  };
-
-  useEffect(() => {
-    if (!refVideo.current) {
-      return;
-    }
-
-    // refVideo.current.srcObject = stream;
-  }, [stream, refVideo]);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={handleRecording}>start</button>
-        <button onClick={handleStop}>stop</button>
-        <button onClick={handleSave}>save</button>
-        {blob && (
-          <video
-            src={URL.createObjectURL(blob)}
-            controls
-            autoPlay
-            ref={refVideo}
-            style={{ width: '700px', margin: '1em' }}
-          />
-        )}
-      </header>
-    </div>
-  );
 }
+export default VideoUploader;
