a1c35b1640050e0e2a032b2e8345fea585ad5654
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-08-07 20:31:06 +0300

    Update VideoUploa

--- src/pages/news/VideoUploader.js
+++ src/pages/news/VideoUploader.js
@@ -1,15 +1,37 @@
-import { useReactMediaRecorder } from "react-media-recorder";
-import React from 'react';
-function VideoUploader(){
-  const { status, startRecording, stopRecording, mediaBlobUrl } = useReactMediaRecorder({ video: true });
+import React, { useState } from 'react';
+import { useReactMediaRecorder } from 'react-media-recorder';
+import Worker from 'worker-loader/lib/worker';
+
+const VideoUploader = () => {
+  const [status, setStatus] = useState('');
+  const [recording, setRecording] = useState(false);
+  const [mediaBlobUrl, setMediaBlobUrl] = useState('');
+
+  const { startRecording, stopRecording } = useReactMediaRecorder({ video: true });
+
+  const onRecord = () => {
+    if (!recording) {
+      startRecording();
+      setStatus('Recording...');
+    }
+  };
+
+  const onStop = () => {
+    if (recording) {
+      stopRecording();
+      setStatus('Stopped recording');
+      setMediaBlobUrl(useReactMediaRecorder.getBlobUrl());
+    }
+  };
 
   return (
     <div>
       <p>{status}</p>
-      <button onClick={startRecording}>Start Recording</button>
-      <button onClick={stopRecording}>Stop Recording</button>
+      <button onClick={onRecord}>Start Recording</button>
+      <button onClick={onStop}>Stop Recording</button>
       <video src={mediaBlobUrl} controls autoPlay loop />
     </div>
   );
 };
+
 export default VideoUploader;
