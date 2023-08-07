b4cf66ffc6c7fb851c7e1fec8e6d0f34d148f6d1
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-08-08 00:31:23 +0300

    Update VideoUploader.js

--- src/pages/news/VideoUploader.js
+++ src/pages/news/VideoUploader.js
@@ -2,17 +2,7 @@ import { ReactMediaRecorder } from "react-media-recorder";
 import React from 'react';
 const VideoUploader = () => (
   <div>
-    <ReactMediaRecorder
-      video
-      render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
-        <div>
-          <p>{status}</p>
-          <button onClick={startRecording}>Start Recording</button>
-          <button onClick={stopRecording}>Stop Recording</button>
-          <video src={mediaBlobUrl} controls autoPlay loop />
-        </div>
-      )}
-    />
+    null
   </div>
 );
 export default VideoUploader;
