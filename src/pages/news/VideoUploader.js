d139e0225dae113093058d124af2be6220b5e778
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-08-08 00:24:59 +0300

    Update VideoUploader.js

--- src/pages/news/VideoUploader.js
+++ src/pages/news/VideoUploader.js
@@ -1,53 +1,18 @@
-import React, { useState } from "react";
 import { ReactMediaRecorder } from "react-media-recorder";
-
-function VideoUploader() {
-  const [recording, setRecording] = useState(false);
-  const [recordedBlob, setRecordedBlob] = useState(null);
-
-  const handleRecordingStart = () => {
-    setRecording(true);
-    setRecordedBlob(null);
-  };
-
-  const handleRecordingStop = (blobUrl) => {
-    setRecording(false);
-    setRecordedBlob(blobUrl);
-  };
-
-  return (
-    <div>
-      <h2>Video Recorder</h2>
-      <ReactMediaRecorder
-        video
-        render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
-          <div>
-            {status === "idle" ? (
-              <button onClick={startRecording} disabled={recording}>
-                Start Recording
-              </button>
-            ) : (
-              <button onClick={stopRecording} disabled={!recording}>
-                Stop Recording
-              </button>
-            )}
-
-            {/* Show the real-time recording preview */}
-            {status === "recording" && <video src={mediaBlobUrl} autoPlay loop />}
-
-            {/* Show the recorded video after recording */}
-            {recordedBlob && (
-              <div>
-                <video src={recordedBlob} controls />
-              </div>
-            )}
-          </div>
-        )}
-        onStart={handleRecordingStart}
-        onStop={handleRecordingStop}
-      />
-    </div>
-  );
-}
-
+import React from 'react';
+const VideoUploader = () => (
+  <div>
+    <ReactMediaRecorder
+      video
+      render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+        <div>
+          <p>{status}</p>
+          <button onClick={startRecording}>Start Recording</button>
+          <button onClick={stopRecording}>Stop Recording</button>
+          <video src={mediaBlobUrl} controls autoPlay loop />
+        </div>
+      )}
+    />
+  </div>
+);
 export default VideoUploader;
