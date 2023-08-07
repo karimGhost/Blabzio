255921f3411cb7a29785ee4b14fce7f463297989
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-08-07 20:29:36 +0000

    rebase

---
+++ gatsby-node.js
@@ -0,0 +1,11 @@
+const path = require('path');
+exports.onCreateWebpackConfig = ({ actions, loaders, getConfig }) => {
+      const config = getConfig();
+        // Add worker-loader rule for web workers
+          config.module.rules.push({
+              test: /\.worker\.js$/,
+                  use: loaders.worker(),
+                    });
+                      actions.replaceWebpackConfig(config);
+                      };
+
