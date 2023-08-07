b5b0828214ef26ea6449dd6736cb778315ca3954
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-08-07 22:00:07 +0300

    Update VideoJS.js

--- src/pages/news/Posted/VideoJS.js
+++ src/pages/news/Posted/VideoJS.js
@@ -1,38 +1,10 @@
 import React from 'react';
-import videojs from 'video.js';
-const VideoJS = (props) => {
-   const videoReference = React.useRef(null);
-   const playerReference = React.useRef(null);
-   const videoRef = React.useRef(null)
-   const {options, onReady} = props;
-   React.useEffect(() => {
-      // Initializing video.js player
-      if (!playerReference.current) {
-         const videoElement = videoReference.current;
-         if (!videoElement) return;
-         const player = playerReference.current =
-         videojs(videoElement, options, () => {
-            videojs.log('Video player is ready');
-            onReady && onReady(player);
-         });
-      }
-   }, [options, videoReference]);
 
-   // Destroy video.js player on component unmount
-   React.useEffect(() => {
-      const player = playerReference.current;
-      return () => {
-         if (player) {
-            player.dispose();
-            playerReference.current = null;
-         }
-      };
-   }, [playerReference]);
-   // wrap player with data-vjs-player` attribute
-   // so no additional wrapper are created in the DOM
+const VideoJS = () => {
+    
    return (
-      <div data-vjs-player>
-         <video ref={videoRef} className='video-js vjs-big-playcentered'/>
+      <div>
+         
       </div>
    );
 }
