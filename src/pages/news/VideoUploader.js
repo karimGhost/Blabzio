13903747cb9e04986f357eb4be9ecc4b0d9d096c
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-08-07 20:39:42 +0300

    Update VideoUploader.js

--- src/pages/news/VideoUploader.js
+++ src/pages/news/VideoUploader.js
@@ -1,35 +1,13 @@
 import React, { useState } from 'react';
-import { useReactMediaRecorder } from 'react-media-recorder';
+
 //import Worker from 'worker-loader/lib/worker';
 
 const VideoUploader = () => {
-  const [status, setStatus] = useState('');
-  const [recording, setRecording] = useState(false);
-  const [mediaBlobUrl, setMediaBlobUrl] = useState('');
-
-  const { startRecording, stopRecording } = useReactMediaRecorder({ video: true });
-
-  const onRecord = () => {
-    if (!recording) {
-      startRecording();
-      setStatus('Recording...');
-    }
-  };
-
-  const onStop = () => {
-    if (recording) {
-      stopRecording();
-      setStatus('Stopped recording');
-      setMediaBlobUrl(useReactMediaRecorder.getBlobUrl());
-    }
-  };
-
+  
   return (
     <div>
-      <p>{status}</p>
-      <button onClick={onRecord}>Start Recording</button>
-      <button onClick={onStop}>Stop Recording</button>
-      <video src={mediaBlobUrl} controls autoPlay loop />
+ used to work
+      
     </div>
   );
 };
