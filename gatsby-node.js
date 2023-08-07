fdf37dc509fb38b8a6efde506ed6581a3ff552b8
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-08-08 00:34:35 +0300

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,15 +1,17 @@
 const path = require('path');
-exports.onCreateWebpackConfig = ({ actions, loaders, getConfig }) => {
+
+exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
   const config = getConfig();
-  
+
   // Add worker-loader rule for web workers
   config.module.rules.push({
     test: /\.worker\.js$/,
-    use: {
-      loader: 'worker-loader',
-    },
+    use: [
+      {
+        loader: 'worker-loader',
+      },
+    ],
   });
 
   actions.replaceWebpackConfig(config);
 };
-
