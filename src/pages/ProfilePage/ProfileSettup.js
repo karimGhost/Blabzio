e67254841cddc258e49c8b12841dd69b19546be1
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-08-12 22:37:45 +0300

    Update ProfileSettup.js

--- src/pages/ProfilePage/ProfileSettup.js
+++ src/pages/ProfilePage/ProfileSettup.js
@@ -52,12 +52,278 @@ const firebaseConfig222 ={
 
 }
 
+export default function Profile(props) {
 
-export default function Profiles(props) {
-return(
-  <div>
+
+  firebase.initializeApp(firebaseConfig2, 'app014');
+
+  /*const [actives, setactive] = useState(false); */
+
+ /*
+ const actives = props.actives;
+ const  setactive = props.setactive;
+*/
+  const app4 = firebase.app('app014');
+  const { user,setUser, loading,signOut } = useAuth();
+  const auth = app4.auth();
+  const database = app4.database();
+
+  const [language, setLanguage] = useState('');
+  const [favorites, setFavorites] = useState([]);
+  const [profileImg, setProfileImg] = useState(im);
+
+  const [Darks, setDarks] = useState(false)
+
+
+  useEffect(() => {
+    app4.auth().onAuthStateChanged(currentUser => {
+      if (currentUser) {
+        setUser(currentUser);
+        database.ref(`users/${currentUser.uid}`).on('value', snap => {
+
+       if(snap.val()){
+          setProfileImg(snap.val().profileImg);
+          setLanguage(snap.val().language);
+  setFavorites(snap.val().favorites);
+       }      
+});
+
+      }
+    });
+
+
+
+
+  }, []);
+
+
+  useEffect(() => {
+    // Only run the effect if the user object is defined
+    if (user) {
+      app4.database().ref(`profiles/${user.uid}`).on('value', snapshot => {
+        console.log(JSON.stringify(snapshot.val()))
+        if (!snapshot.val()) {
+
+          console.log("Snapshot value is null or undefined");
+          return;
+        }
+
+
+        // check if snapshot.val() is an object
+        if(typeof snapshot.val() === 'object'){
+          const val = snapshot.val();
+         setProfileImg(val.profileImg)
+        } else {
+setProfileImg(snapshot.val())
+        }
+      });
+    }
+
+
+
+  }, [user]);
+
+
+
+
+
+  console.log(`ìtsb ${profileImg}`)
+
+  useEffect(() => {
+
+    let vf = document.getElementById('#va');
+    user &&
+    vf.addEventListener('mouseover', () => {
+      alert('h')
+      document.querySelector('.profileimg').classNameNameList.add('propo')
+    }) 
+  }, [])
+
+  const handleLogout = async () => {
+    try {
+      signOut();
+      setUser(null);
+      sessionStorage.clear();
+    } catch (error) {
+      console.error(error);
+    }
+
+
+
+
+  };
+
+  const handleProfileImgChange = e => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setProfileImg(reader.result);
+      app4.database().ref(`profile/${user.uid}`).set({ profileImg:reader.result })
+
+    };
+    reader.readAsDataURL(file);
+
+
+  };
+
+/*
+  useEffect(() => {
+    if (!user || !actives) return;
+    const v8 = document.querySelector(".v8");
+    const vh = document.querySelector('#dp');
+    vh.addEventListener('mouseover', () => {
     
-  </div>
+      v8.classNameNameList.add("vjj");
+    });
+  
+      vh.addEventListener('mouseout', () => {
+        v8.classNameNameList.remove("vjj");
+      })
+    if(actives){
+    }
+  }, [actives]);
+  /*
+  const bu = props.bu;
+  const setBu =  props.setBu;
+*/
+function active(){
+  props.setactive(true)
+  props.setBu(false)
+
+}
+
+function setActive(){
+  props.setactive(false)
+  props.setBu(true)
+}
+
+ /* margin-top:20px;
+background-color:#f2f6fc;
+color:#69707a;*/
+
+const actives = props.actives;
+const setBu = props.setBu;
+const bu = props.bu 
+function isActive({ isCurrent }) {
+  return isCurrent ? { className: 'active'} : null;
+}
+
+
+
+/*     {user && !actives && <Profile  actives={actives} setactive={setactive} bu={props.bu} setBu={props.setBu} setIsOpen={props.setIsOpen} language={props.language} handleLanguageChange={props.handleLanguageChange} />}
+                               {user && !actives && <Profile  actives={actives} setactive={setactive} bu={props.bu} setBu={props.setBu} setIsOpen={props.setIsOpen} language={props.language} handleLanguageChange={props.handleLanguageChange} />}
+   */
+
+
+
+
+  return (
+    <div>
+
+
+
+{!user && !actives &&
+  <img src={profileImg} className="round dr mt-2" style={{borderRadius: "50%"}}  height="50" 
+  alt="profile" loading="lazy" />
+}
+
+
+<div style={{height: "100%", overflowY: "scroll"}} className={`{${Darks && "darken"} 'Bgg' }`}>
+<div class="container-xl px-4 mt-4">
+
+<Navbar setDarks={setDarks} />
+
+  {/* Account page navigation*/}
+
+  <div className="row">
+
+
+<div className="col-xl-4">
+  {/* Profile picture card*/}
+    <div className="card mb-4 mb-xl-0">
+        <div className="card-header">Profile Picture</div>
+        <div className="card-body text-center">
+          {/* Profile picture image*/}
+            <img className="img-account-profile rounded-circle mb-2" src={profileImg} alt="" />
+          {/* Profile picture help block*/}
+            <div className="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
+          {/* Profile picture upload button*/}
+            <button className="btn btn-primary" type="button">Upload new image</button>
+    </div>
+    </div>
+</div>
+<div className="col-xl-8">
+  {/* Account details card */}
+    <div className="card mb-4">
+        <div className="card-header">Account Details</div>
+        <div className="card-body">
+            <form>
+              {/* Form Group (username)*/}
+                <div className="mb-3">
+                    <label className="small mb-1" for="inputUsername">Username (how your name will appear to other users on the site)</label>
+                    <input className="form-control" id="inputUsername" type="text" placeholder="Enter your username" value="username"/>
+                </div>
+              {/* Form Row*/}
+                <div className="row gx-3 mb-3">
+                  {/* Form Group (first name)*/}
+                    <div className="col-md-6">
+                        <label className="small mb-1" for="inputFirstName">First name</label>
+                        <input className="form-control" id="inputFirstName" type="text" placeholder="Enter your first name" value="Valerie"/>
+                    </div>
+                  {/* Form Group (last name)*/}
+                    <div className="col-md-6">
+                        <label className="small mb-1" for="inputLastName">Last name</label>
+                        <input className="form-control" id="inputLastName" type="text" placeholder="Enter your last name" value="Luna"/>
+                    </div>
+                </div>
+              {/* Form Row        */}
+                <div className="row gx-3 mb-3">
+                  {/* Form Group (organization name)*/}
+                    <div className="col-md-6">
+                        <label className="small mb-1" for="inputOrgName">Organization name</label>
+                        <input className="form-control" id="inputOrgName" type="text" placeholder="Enter your organization name" value="Start Bootstrap"/>
+                    </div>
+                  {/* Form Group (location)*/}
+                    <div className="col-md-6">
+                        <label className="small mb-1" for="inputLocation">Location</label>
+                        <input className="form-control" id="inputLocation" type="text" placeholder="Enter your location" value="San Francisco, CA"/>
+                    </div>
+                </div>
+              {/* Form Group (email address)*/}
+                <div className="mb-3">
+                    <label className="small mb-1" for="inputEmailAddress">Email address</label>
+                    <input className="form-control" id="inputEmailAddress" type="email" placeholder="Enter your email address" value="name@example.com"/>
+                </div>
+              {/* Form Row*/}
+                <div className="row gx-3 mb-3">
+                  {/* Form Group (phone number)*/}
+                    <div className="col-md-6">
+                        <label className="small mb-1" for="inputPhone">Phone number</label>
+                        <input className="form-control" id="inputPhone" type="tel" placeholder="Enter your phone number" value="555-123-4567"/>
+                    </div>
+                  {/* Form Group (birthday)*/}
+                    <div className="col-md-6">
+                        <label className="small mb-1" for="inputBirthday">Birthday</label>
+                        <input className="form-control" id="inputBirthday" type="text" name="birthday" placeholder="Enter your birthday" value="06/10/1988"/>
+                    </div>
+                </div>
+              {/* Save changes button*/}
+                <button className="btn btn-primary" type="button">Save changes</button>
+            </form>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+
+</div>
+</div>
+
+
+
+
+
+
 );}
 
 export const Head = () => <title>ProfileSetup</title>
