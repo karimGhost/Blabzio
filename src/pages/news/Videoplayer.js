79032db3409cbd2b0b3c403e5634f25f889b8799
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-08-12 23:50:13 +0300

    commmit vplayer

--- src/pages/news/Videoplayer.js
+++ src/pages/news/Videoplayer.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaPlay, FaPause, FaHeart, FaThumbsUp, FaThumbsDown, FaComment, FaShare } from 'react-icons/fa';
 import { nanoid } from 'nanoid';
 import video from "../../images/anim.mp4";
+import Video from "../../styles/Video.scss";
 const Videoplayer = () => {
   const [playing, setPlaying] = useState(false);
   const [likes, setLikes] = useState(0);
@@ -25,114 +26,226 @@ const Videoplayer = () => {
     setComments([...comments, newComment]);
   };
 
+
+
+  const comments = [
+    // ... your comment objects here
+  ];
+  let likesAmount = '';
+
+  const [video, setVideo] = useState(null);
+  const [barWidth, setBarWidth] = useState(0);
+
+  useEffect(() => {
+    const videoElement = document.querySelector('.video');
+    setVideo(videoElement);
+  }, []);
+
+  const displayTime = (time) => {
+    const mins = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds <= 9 ? `0${seconds}` : seconds;
+    return `${mins}:${seconds}`;
+  };
+
+  const updateProgress = () => {
+    if (video) {
+      const currentTime = video.currentTime;
+      const videoDuration = video.duration;
+      setBarWidth((currentTime / videoDuration) * 100);
+    }
+  };
+
+  const setProgress = (e) => {
+    if (video) {
+      const range = document.querySelector('.progress-range');
+      if (range) {
+        const time = e.nativeEvent.offsetX / range.offsetWidth;
+        video.currentTime = time * video.duration;
+      }
+    }
+  };
+
+  const activateComments = () => {
+    const commentsContainer = document.querySelector('.comments-container');
+    if (video && commentsContainer) {
+      commentsContainer.classList.add('comments-active');
+      video.pause();
+    }
+  };
+
+  const deactivateComments = () => {
+    const commentsContainer = document.querySelector('.comments-container');
+    if (video && commentsContainer) {
+      commentsContainer.classList.remove('comments-active');
+      video.play();
+    }
+  };
+
+  const loadComments = () => {
+    const commentsList = document.querySelector('.comments-list');
+    const commentsCount = document.querySelector('.comments-head-label');
+    const commentsCount2 = document.querySelector('.comments');
+
+    if (commentsList && commentsCount && commentsCount2) {
+      commentsList.innerHTML = '';
+      commentsCount.textContent = `${comments.length} comments`;
+      commentsCount2.textContent = `${comments.length}`;
+
+      comments.forEach((comment) => {
+        const html = `<div class="comments-item">
+          <span class="comment-top">
+            <span class="comment-top-logo" style="background-image:url(${comment.profilePhoto})"></span>
+            <span class="comment-top-details">
+              <span class="user-name">${comment.userName}</span>
+              <span class="user-time">${comment.timePosted}</span>
+              <span class="user-comment">${comment.comment}</span>
+            </span>
+          </span>
+        </div>`;
+        commentsList.insertAdjacentHTML('afterbegin', html);
+      });
+
+      likesAmount = Number(likes.dataset.likes);
+    }
+  };
+
+  const updateLikes = () => {
+    const likes = document.querySelector('.likes');
+    const likesIcon = document.getElementById('likes-icon');
+
+    if (likesAmount >= 1000) return;
+    likesIcon.src = 'https://assets.codepen.io/2629920/heart+%281%29.png';
+    likesAmount = likesAmount + 1;
+    likes.textContent = `${likesAmount === 1000 ? '1K' : likesAmount}`;
+  };
+
+  useEffect(() => {
+    if (video) {
+      video.addEventListener('timeupdate', updateProgress);
+      video.addEventListener('canplay', updateProgress);
+    }
+
+    return () => {
+      if (video) {
+        video.removeEventListener('timeupdate', updateProgress);
+        video.removeEventListener('canplay', updateProgress);
+      }
+    };
+  }, [video]);
+
+  useEffect(() => {
+    updateProgress();
+  }, [video]);
+
   return (
-  	<div class="container">
-		<div class="overlay">
-			<div class="howto">
-				<div class="explain">
-					<ul class="explain-list">
-						<li class="explain-details">
-							<h2 class="explain-details-head">Likes</h2>
-							<span class="explain-details-desc">Click the <span class="icon">
-									<img src="https://assets.codepen.io/2629920/heart.png" alt="">
+  	<div className={Video.container}>
+		<div className={Video.overlay}>
+			<div className={Video.howto}>
+				<div className={Video.explain}>
+					<ul className={Video.explain-list}>
+						<li className={Video.explain-details}>
+							<h2 className={Video.explain-details-head}>Likes</h2>
+							<span className={Video.explain-details-desc}>Click the <span className={Video.icon}>
+									<img src="https://assets.codepen.io/2629920/heart.png" alt=""/>
 								</span> to set a like on the video.</span>
 						</li>
-						<li class="explain-details">
-							<h2 class="explain-details-head">Comments</h2>
-							<span class="explain-details-desc">Click the <span class="icon">
-									<img src="https://assets.codepen.io/2629920/chat.png" alt="">
+						<li className={Video.explain-details}>
+							<h2 className={Video.explain-details-head}>Comments</h2>
+							<span className={Video.explain-details-desc}>Click the <span className={Video.icon}>
+									<img src="https://assets.codepen.io/2629920/chat.png" alt=""/>
 								</span>view comments.</span>
 						</li>
 					</ul>
-					<div class="explain-video">
-						<h2 class="explain-details-head explain-video">Video</h2>
-						<div class="explain-video-bar">
-							<div class="explain-video-bar-w"></div>
+					<div className={Video.explain-video}>
+						<h2 className={Video.explain-details-head, Video.explain-video}>Video</h2>
+						<div className={Video.explain-video-bar}>
+							<div className={Video.explain-video-bar-w}></div>
 						</div>
-						<span class="explain-details-desc">Click through the video to navigate to specific time line.</span>
+						<span className={Video.explain-details-desc}>Click through the video to navigate to specific time line.</span>
 					</div>
 				</div>
-				<button class="howto-close">Got it</button>
+				<button className={Video.howto-close}>Got it</button>
 			</div>
 		</div>
-		<div class="comments-container">
-			<div class="comments-head">
-				<span class="comments-head-label"></span>
-				<span class="comments-head-close">
+		<div className={Video.comments-container}>
+			<div className={Video.comments-head}>
+				<span className={Video.comments-head-label}></span>
+				<span className={Video.comments-head-close}>
 					&#10005;
 				</span>
 			</div>
-			<div class="comments-list">
+			<div className={Video.comments-list}>
 			</div>
 		</div>
-		<video src="https://assets.codepen.io/2629920/gt3.mp4" class="video" playsinline autoplay loop muted></video>
+		<video src="https://assets.codepen.io/2629920/gt3.mp4" className={Video.video} playsinline autoplay loop muted></video>
 
-		<!-- 	top labels	 -->
-		<div class="top">
-			<span class="label label-main">Following</span>
-			<span class="label label-active">For You</span>
+		{/* 	top labels	*/}
+		<div className={top}>
+			<span className={Video.label, Video.label-main}>Following</span>
+			<span className={Video.label, Video.label-active}>For You</span>
 		</div>
 
-		<!-- right labels	 -->
-		<div class="right">
-			<div class="icons-item right-icon">
-				<span class="icon">
-					<img src="https://assets.codepen.io/2629920/heart.png" alt="" id="likes-icon">
+		{/* right labels	*/ }
+		<div className={Video.right}>
+			<div className={Video.icons-item,Video.right-icon}>
+				<span className={Video.icon}>
+					<img src="https://assets.codepen.io/2629920/heart.png" alt="" id="likes-icon"/>
 				</span>
-				<span class="icon-label likes right-label" data-likes='999'>999</span>
+				<span className={Video.icon-label, Video.likes, Video.right-label} data-likes='999'>999</span>
 			</div>
-			<div class="icons-item right-icon">
-				<span class="icon">
-					<img src="https://assets.codepen.io/2629920/chat.png" alt="" id="comments-icon">
+			<div className={Video.icons-item, Video.right-icon}>
+				<span className={Video.icon}>
+					<img src="https://assets.codepen.io/2629920/chat.png" alt="" id="comments-icon"/>
 				</span>
-				<span class="icon-label comments right-label"></span>
+				<span className={Video.icon-label, Video.comments, Video.right-label}></span>
 			</div>
-			<div class="icons-item right-icon">
-				<span class="icon">
-					<img src="https://assets.codepen.io/2629920/share.png" alt="">
+			<div className={Video.icons-item, Video.right-icon}>
+				<span className={Video.icon}>
+					<img src="https://assets.codepen.io/2629920/share.png" alt=""/>
 				</span>
-				<span class="icon-label shares right-label">94</span>
+				<span className={Video.icon-label, Video.shares, Video.right-label}>94</span>
 			</div>
 
 		</div>
 
-		<!-- 	bottom labels	 -->
-		<div class="bottom">
-			<span class="progress-duration"></span>
-			<div class="progress-range" title="seek">
-				<div class="progress-bar"></div>
+		{/* 	bottom labels	*/}
+		<div className={Video.bottom}>
+			<span className={Video.progress-duration}></span>
+			<div className={Video.progress-range} title="seek">
+				<div className={Video.progress-bar}></div>
 			</div>
-			<div class="icons">
-				<div class="icons-item">
-					<span class="icon">
-						<img src="https://assets.codepen.io/2629920/home.png" alt="">
+			<div className={Video.icons}>
+				<div className={Video.icons-item}>
+					<span className={Video.icon}>
+						<img src="https://assets.codepen.io/2629920/home.png" alt=""/>
 					</span>
-					<span class="icon-label">Home</span>
+					<span className={Video.icon-label}>Home</span>
 				</div>
-				<div class="icons-item">
-					<span class="icon">
-						<img src="https://assets.codepen.io/2629920/search_1.png" alt="">
+				<div className={Video.icons-item}>
+					<span className={Video.icon}>
+						<img src="https://assets.codepen.io/2629920/search_1.png" alt=""/>
 					</span>
-					<span class="icon-label">Discover</span>
+					<span className={Video.icon-label}>Discover</span>
 				</div>
-				<div class="icons-item">
-					<div class="icons-item-add">
-						<span class="icon" style="display: flex;">
-							<img src=" https://assets.codepen.io/2629920/add.png" alt="">
+				<div className={Video.icons-item}>
+					<div className={Video.icons-item-add}>
+						<span className={Video.icon} style="display: flex;">
+							<img src=" https://assets.codepen.io/2629920/add.png" alt=""/>
 						</span>
 					</div>
 				</div>
-				<div class="icons-item">
-					<span class="icon">
-						<img src="https://assets.codepen.io/2629920/comment.png" alt="">
+				<div className={Video.icons-item}>
+					<span className={Video.icon}>
+						<img src="https://assets.codepen.io/2629920/comment.png" alt=""/>
 					</span>
-					<span class="icon-label">Inbox</span>
+					<span className={Video.icon-label}>Inbox</span>
 				</div>
-				<div class="icons-item">
-					<span class="icon">
-						<img src="https://assets.codepen.io/2629920/user+%281%29.png" alt="">
+				<div className={Video.icons-item}>
+					<span className={Video.icon}>
+						<img src="https://assets.codepen.io/2629920/user+%281%29.png" alt=""/>
 					</span>
-					<span class="icon-label">Me</span>
+					<span className={Video.icon-label}>Me</span>
 				</div>
 			</div>
 		</div>
