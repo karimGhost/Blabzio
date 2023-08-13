453e83daa9ca7a2b8890f37a11ca351108eab17a
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-08-13 21:50:28 +0000

    commitZ

--- src/pages/news/TheWall.js
+++ src/pages/news/TheWall.js
@@ -12,7 +12,7 @@ export default function TheWall() {
 
   <main style={{marginTop:"150px"}}>
     <Navbar />
-    <button style={{position:"fixed", top:"110px",zIndex:"20", margin:"0 20%", left:"0", right: "0", borderRadius:"15px", }} >Record Video +</button>
+    <button style={{position:"fixed", top:"110px",zIndex:"5", margin:"0 20%", left:"0", right: "0", borderRadius:"15px", }} >Record Video +</button>
     <div className='width:"fit-content ", height:"fit-content ", marginTop:"50px"'>
         <VideoPlayer />
 
