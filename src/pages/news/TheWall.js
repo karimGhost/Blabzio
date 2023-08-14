293ee0253128464c3824c73de40b06d0063cc04c
karimGhost <74273577+karimGhost@users.noreply.github.com>
2023-08-14 17:31:41 +0000

    refadded

--- src/pages/news/TheWall.js
+++ src/pages/news/TheWall.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 
 import VideoPlayer from './Videoplayer';
 import VideoRecorder from './VideoRecorder';
